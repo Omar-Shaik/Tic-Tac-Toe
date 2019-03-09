2e5c619bc53bb0997913b3c5bd473939592999df
Sharko <35842212+Oshaik@users.noreply.github.com>
2019-03-09 14:42:08 -0500

    Update node.py

--- node.py
+++ node.py
@@ -6,10 +6,19 @@ class Node:
         self.children = []
         self.mark = "X"
         self.set_mark()
-        self.create_children()
 
     def set_mark(self):
         if self.height % 2 == 1:
             self.mark = "O"
-
+    
+    def create_children(node):
+        if node.height < 9:
+            i = 0
+            while i < len(node.board):
+                local_board = node.board
+                if node.board[i] == "":
+                    local_board[i] = node.mark
+                n = Node(local_board, node.height+1)
+                node.children.append(n)
+                i += 1
 
