a652cc2ee47ba93b56040d8558ec120bccc046c5
Sohaib Mohiuddin <43653130+sm131@users.noreply.github.com>
2019-03-10 13:33:08 -0400

    Update node.py

--- node.py
+++ node.py
@@ -1,12 +1,7 @@
 class Node:
-    def __init__(self, new_board, height, count):
-        self.board = [] + new_board
-        self.height = height + 1
-        self.count = count
-        self.count[0] += 1
-        print(count)
-        print("Hi, I am " + str(self.height) + " nodes deep")
-        print(self.board)
+    def __init__(self, new_board, height):
+        self.board = new_board
+        self.height = height
         self.score = None
         self.children = []
         self.mark = "X"
@@ -16,15 +11,13 @@ class Node:
         if self.height % 2 == 1:
             self.mark = "O"
 
-    def create_children(self):
-        if self.height < 9:
+    def create_children(node):
+        if node.height < 9:
             i = 0
-            while i < len(self.board):
-                local_board = [] + self.board
-                if self.board[i] == "":
-                    local_board[i] = self.mark
-                    n = Node(local_board, 0+self.height, self.count)
-                    n.create_children()
-                    self.children.append(n)
+            while i < len(node.board):
+                local_board = node.board
+                if node.board[i] == "":
+                    local_board[i] = node.mark
+                n = Node(local_board, node.height+1)
+                node.children.append(n)
                 i += 1
-
