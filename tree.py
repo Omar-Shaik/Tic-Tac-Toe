827b5614579ad17b9cbaad58fa76b39f3ca308b5
Sohaib Mohiuddin <43653130+sm131@users.noreply.github.com>
2019-03-10 13:32:38 -0400

    Update tree.py

--- tree.py
+++ tree.py
@@ -1,14 +1,15 @@
 from node import Node
+from tkinter import messagebox
 
 class Tree:
     def __init__(self):
         self.board = ["","","","","","","","",""]
-        self.root = Node(self.board,0)
-        self.root.set_children()
+        self.root = Node(self.board, 0)
+        self.root.create_children()
         self.set_score(self.root)
 
 
-    def set_score(node):
+    def set_score(self, node):
         if(node.board[0] == "X" and node.board[1] == "X" and node.board[2] == "X" or
             node.board[3] == "X" and node.board[4] == "X" and node.board[5] == "X" or
             node.board[6] == "X" and node.board[7] == "X" and node.board[8] == "X" or
@@ -17,7 +18,8 @@ class Tree:
             node.board[2] == "X" and node.board[5] == "X" and node.board[8] == "X" or
             node.board[0] == "X" and node.board[4] == "X" and node.board[8] == "X" or
             node.board[2] == "X" and node.board[4] == "X" and node.board[6] == "X"):
-            node.weight = 1
+            node.weight = 1 #WHERE DID ".weight" COME FROM? CANT FIND THE WORD "weight" ANYWHERE IN THE CODE
+            
         elif(node.board[0] == "O" and node.board[1] == "O" and node.board[2] == "O" or
           node.board[3] == "O" and node.board[4] == "O" and node.board[5] == "O" or
           node.board[6] == "O" and node.board[7] == "O" and node.board[8] == "O" or
@@ -27,6 +29,7 @@ class Tree:
           node.board[0] == "O" and node.board[4] == "O" and node.board[8] == "O" or
           node.board[2] == "O" and node.board[4] == "O" and node.board[6] == "O"):
           node.weight = -1
+          
         else:
             if node.height == 9:
                 node.weight = 0
@@ -36,3 +39,10 @@ class Tree:
                 elif node.mark == "O":
                     node.score = min(map(set_score,node.children))
         return node.score
+    
+    def winx(self):
+        if (self.set_score == True):
+            messagebox.showinfo("Win", "X is the Winner")
+    def wino(self):
+        if (self.set_score == True):
+            messagebox.showinfo("Win", "O is the Winner")
