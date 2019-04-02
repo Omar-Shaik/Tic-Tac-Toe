1fa8b759948330f5bdfed13367014258d8c8115c
Sharko <35842212+Oshaik@users.noreply.github.com>
2019-04-02 09:45:48 -0400

    Create game.py

---
+++ game.py
@@ -0,0 +1,59 @@
+#This function works.
+#Function a boolean that's True if a game is finished.
+def finished(a):
+    done = True
+    for x in a:
+        if x == ' ':
+            done = False
+    if ((a[0] == 'X' and a[1] == 'X' and a[2] =='X') or
+    (a[3] == 'X' and a[4] == 'X' and a[5] =='X') or
+    (a[6] == 'X' and a[7] == 'X' and a[8] =='X') or
+    (a[0] == 'X' and a[3] == 'X' and a[6] =='X') or
+    (a[1] == 'X' and a[4] == 'X' and a[7] =='X') or
+    (a[2] == 'X' and a[5] == 'X' and a[8] =='X') or
+    (a[0] == 'X' and a[4] == 'X' and a[8] =='X') or
+    (a[2] == 'X' and a[4] == 'X' and a[6] =='X')):
+        done = True
+    if ((a[0] == 'O' and a[1] == 'O' and a[2] =='O') or
+    (a[3] == 'O' and a[4] == 'O' and a[5] =='O') or
+    (a[6] == 'O' and a[7] == 'O' and a[8] =='O') or
+    (a[0] == 'O' and a[3] == 'O' and a[6] =='O') or
+    (a[1] == 'O' and a[4] == 'O' and a[7] =='O') or
+    (a[2] == 'O' and a[5] == 'O' and a[8] =='O') or
+    (a[0] == 'O' and a[4] == 'O' and a[8] =='O') or
+    (a[2] == 'O' and a[4] == 'O' and a[6] =='O')):
+        done = True
+    return done
+
+#THis function works.
+#Function that returns the value of a finished game.
+def win(a):
+    if ((a[0] == 'X' and a[1] == 'X' and a[2] =='X') or
+    (a[3] == 'X' and a[4] == 'X' and a[5] =='X') or
+    (a[6] == 'X' and a[7] == 'X' and a[8] =='X') or
+    (a[0] == 'X' and a[3] == 'X' and a[6] =='X') or
+    (a[1] == 'X' and a[4] == 'X' and a[7] =='X') or
+    (a[2] == 'X' and a[5] == 'X' and a[8] =='X') or
+    (a[0] == 'X' and a[4] == 'X' and a[8] =='X') or
+    (a[2] == 'X' and a[4] == 'X' and a[6] =='X')):
+        return 1
+    elif ((a[0] == 'O' and a[1] == 'O' and a[2] =='O') or
+    (a[3] == 'O' and a[4] == 'O' and a[5] =='O') or
+    (a[6] == 'O' and a[7] == 'O' and a[8] =='O') or
+    (a[0] == 'O' and a[3] == 'O' and a[6] =='O') or
+    (a[1] == 'O' and a[4] == 'O' and a[7] =='O') or
+    (a[2] == 'O' and a[5] == 'O' and a[8] =='O') or
+    (a[0] == 'O' and a[4] == 'O' and a[8] =='O') or
+    (a[2] == 'O' and a[4] == 'O' and a[6] =='O')):
+        return -1
+    else:
+        return 0
+
+#This function works.
+#Function that returns the depth of an array, a.
+def find_depth(a):
+    depth = 0
+    for x in a:
+        if x != ' ':
+            depth += 1
+    return depth
